8841a56909789d1504e183c6613dea5217b8a6e7
GeriiGarcia <gerardgarciagros@gmail.com>
2024-05-19 00:27:51 +0200

    gitignore

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,13 +4,9 @@
 
 import FlutterMacOS
 import Foundation
-import connectivity_plus
+
 import path_provider_foundation
-import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-
 }
