4240d4db99503077e25324901c38e20e59c6273a
Mario Lacueva <mariolacueva01@gmail.com>
2024-05-18 21:26:16 +0200

    getters para escoger viaje + json data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -48,68 +43,128 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _dataFromApi = 'Loading...';
+  String _location = 'Location: Loading...';
+  List<String> _uniqueRouteShortNames = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<List<dynamic>> _loadData() async {
+    try {
+      final String scheduleRaw = await rootBundle.loadString('assets/data/schedule.json');
+      final List<dynamic> scheduleList = json.decode(scheduleRaw);
+      return scheduleList;
+    } catch (e) {
+      throw Exception('Error loading data');
+    }
+  }
+
+  List<String> _getLines(List<dynamic> scheduleList) {
+    final List<String> lines = scheduleList.map((item) => item['route_short_name'] as String).toList();
+    return lines.toList();
+  }
+
+  List<String> getStopNamesForRoute(String line, List<dynamic> scheduleList) {
+    final stops = scheduleList.where((item) => item['route_short_name'] == line)
+                              .map((item) => item['stop_name'] as String)
+                              .toList();
+    return stops;
+  }
+
+  List<String> getDestinations(String line, List<dynamic> scheduleList) {
+    final dest = scheduleList.where((item) => item['route_short_name'] == line)
+                              .map((item) => item['trip_headsign'] as String)
+                              .toList();
+    return dest;
+  }
+
+/*
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _location = 'Location services are disabled.';
+      });
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _location = 'Location permissions are denied';
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _location = 'Location permissions are permanently denied, we cannot request permissions.';
+      });
+      return;
+    } 
+
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _location = 'Lat: ${position.latitude}, Lon: ${position.longitude}';
     });
   }
 
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('https://api.example.com/data'));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      setState(() {
+        _dataFromApi = jsonData['message'];
+      });
+    } else {
+      setState(() {
+        _dataFromApi = 'Failed to load data';
+      });
+    }
+  }
+  */
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const SizedBox(height: 20),
+            const Text('Unique Route Short Names:'),
+            _uniqueRouteShortNames.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: _uniqueRouteShortNames.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(_uniqueRouteShortNames[index]),
+                        );
+                      },
+                    ),
+                  )
+                : const Text('Loading...'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
